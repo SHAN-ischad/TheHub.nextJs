a41ff50e435a85b6e42f911afb640c6428f0fac2
Iago Vieira Carvalho <iagovelox@gmail.com>
2026-01-25 23:59:06 -0300

    Primeiro commit. Projeto será refeito em next.Js

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+export default function Page() {
+  return (
+    <main className="h-screen ">
+      <body className="w-screen flex flex-col">
+
+
+      </body>
+    </main>
+  )
+}
